78a070cf5588ef682a7b8746accfea908f2093e9
wancheng8578-lab <wanche.ng8578@gmail.com>
2025-11-10 02:31:35 +0800

    delete unused code

--- src/route/ProtectedRoute.jsx
+++ src/route/ProtectedRoute.jsx
@@ -3,5 +3,5 @@ import { useUser } from '@/context';
 
 export default function ProtectedRoute() {
   const { user } = useUser();
-  return user.id ? <Outlet /> : <Navigate to="/login" replace />;
+  return user?.id ? <Outlet /> : <Navigate to="/login" replace />;
 }
--- src/route/routes.jsx
+++ src/route/routes.jsx
@@ -1,4 +1,3 @@
 import Dashboard from '@/pages/Dashboard';
-import Login from '@/pages/Login';
 
 export const routes = [{ path: `/dashboard`, element: <Dashboard /> }];
